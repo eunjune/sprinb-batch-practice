0e367918d664c112668b4b2b061427f86194b460
eunjune <iws1605@naver.com>
2022-03-20 22:10:36 +0900

    Initial commit

---
+++ src/main/java/com/example/sprinbbatchpractice/SprinbBatchPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinbbatchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbBatchPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinbBatchPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprinbbatchpractice/SprinbBatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinbbatchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbBatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
